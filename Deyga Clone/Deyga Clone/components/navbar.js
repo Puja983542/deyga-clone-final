700583cde31830ccf6e097d77849cd1a8e9b7da1
Thakur Suraj Bhan Singh <99983401+Surajj07@users.noreply.github.com>
2022-03-31 13:57:42 +0530

    Add files via upload

---
+++ Deyga Clone/Deyga Clone/components/navbar.js
@@ -0,0 +1,188 @@
+const navbar = ()=>{
+
+    return `<div>
+    <div>
+       <img src="https://media.istockphoto.com/vectors/india-round-flag-vector-flat-icon-vector-id1032066158?k=20&m=1032066158&s=612x612&w=0&h=TFV2KyDfGzdev0tFaIuFJETbCh5r6M7zMShownx7xb0=" alt=""/>
+       <select>
+           <option value="">INR</option>
+       </select>
+       <div>Blogs</div>
+       <div>Ingredients</div>
+    </div>
+    <div>
+        <img src="https://cdn.shopify.com/s/files/1/0034/7901/1441/files/200x150-01_140x.png?v=1640868807" alt=""/>
+    </div>
+    <div>
+        <input type="text" placeholder="SEARCH PRODUCTS"/>
+        <img src="https://cdn1.iconfinder.com/data/icons/hawcons/32/698956-icon-111-search-512.png" alt=""/>
+         <div id="navbar01">
+             <div id="navbar0">Account</div>
+             <div id="navbar00">
+                 <div>Login</div>
+                 <div>Wishlist</div>
+                 <div>Track Order</div>
+                 <div>Sign Up</div>
+             </div>
+         </div>
+         <div>
+             <div>Cart(</div>
+             <div id="countCartItems">0</div>
+             <div>)</div>
+         </div>
+    </div>
+</div>
+<div>
+   <div>
+       <div>
+           <div id="navbar1">Skin Care</div>
+           <div id="navbar11">
+               <div>Face Cream</div>
+               <div>Body Lotion</div>
+               <div>Face Pack</div>
+               <div>Lip Care</div>
+               <div>Aloevera Gel</div>
+               <div>Face Masks(Gel)</div>
+               <div>Toner</div>
+               <div>Eye Care</div>
+               <div>Body Butter</div>
+               <div>Scrubs</div>
+               <div>Foot Care</div>
+               <div>Skin Serum</div>
+           </div>
+       </div>
+       <div>
+            <div id="navbar2">Hair Care</div>
+            <div id="navbar22">
+                <div>Hair Oil</div>
+                <div>Hair Serum</div>
+                <div>Hair Pack</div>
+                <div>Hair Butter</div>
+                <div>Shampoo Bar</div>
+                <div>Combs</div>
+            </div>
+       </div>
+       <div>
+            <div id="navbar3">Bath & Body</div>
+            <div id="navbar33">
+                <div>Body Lotion</div>
+                <div>Handmade Soap</div>
+                <div>Powder</div>
+                <div>Shampoo Bar</div>
+                <div>Loofah</div>
+            </div>
+       </div>
+       <div>
+            <div id="navbar4">Oral Care</div>
+            <div id="navbar44">
+                <div>Tooth Powder</div>
+                <div>Tooth Brush</div>
+            </div>
+       </div>
+       <div>
+            <div id="navbar5">Wellness</div>
+            <div id="navbar55">
+                <div>Cold Pressed Oil</div>
+                <div>Essential Oil</div>
+            </div>
+       </div>
+       <div>
+            <div id="navbar6">Baby Care</div>
+            <div id="navbar66">
+                <div>Baby Soap</div>
+                <div>Powder</div>
+                <div>Baby Butter</div>
+            </div>
+       </div>
+       <div>
+            <div id="navbar7">Men</div>
+            <div id="navbar77">
+                <div>Men Bath Soap</div>
+                <div>Beard Oil</div>
+                <div>Powder</div>
+                <div>Shampoo Bar</div>
+            </div>
+       </div>
+       <div>
+            <div id="navbar8">Combo</div>
+            <div id="navbar88"></div>
+       </div>
+       <div>
+            <div id="navbar9">Gifting</div>
+            <div id="navbar99">
+                <div>Gift Card</div>
+            </div>
+       </div>
+       <div>
+            <div id="navbar10">Offers</div>
+            <div id="navbar1010"></div>
+       </div>
+   </div>
+</div>
+`
+}
+
+const footer = ()=>{
+    return `<div>We ❤️ Being Friends</div>
+    <div>
+        <img src="https://cdn.shopify.com/s/files/1/0034/7901/1441/t/139/assets/facebook-svg2.svg?v=1619612966" alt=""/>
+        <img src="https://cdn.shopify.com/s/files/1/0034/7901/1441/t/139/assets/twitter-svg2.svg?v=1619612966" alt=""/>
+        <img src="https://cdn.shopify.com/s/files/1/0034/7901/1441/t/139/assets/instagram-svg2.svg?v=1619612966" alt=""/>
+        <img src="https://cdn.shopify.com/s/files/1/0034/7901/1441/t/139/assets/ping-svg2.svg?v=1619612966" alt=""/>
+        <img src="https://cdn.shopify.com/s/files/1/0034/7901/1441/t/139/assets/youtube-svg2.svg?v=1619612966" alt=""/>
+    </div>
+    <div>Choose Pure! Choose Deyga!</div>
+    <div>
+        <div>
+            <div>Contact</div>
+            <div>+91 9159305599</div>
+            <div>enquiry@deyga.in</div>
+            <div>Contact us</div>
+            <div>FOR CORPORATE ORDERS:</div>
+            <div>business@deyga.in</div>
+        </div>
+        <div>
+            <div>Support</div>
+            <div>About us</div>
+            <div>FAQs</div>
+            <div>Privacy policy</div>
+            <div>Terms and Conditions</div>
+            <div>Shipping & Returns</div>
+            <div>Track your order</div>
+        </div>
+        <div>
+            <div>Sign-up to get closer</div>
+            <div>Subscribe to get special offers, free giveaways and once in a lifetime deals</div>
+            <input type="text" placeholder="Enter your email address"/>
+            <button>Subscribe</button>
+        </div>
+    </div>
+    <div>THE ALL-EMBRACING AGE OF HANDCRAFTED & NATURAL PRODUCTS</div>
+    <div>Discovering authenticity is now at the heart of every choice of purchase. Modern man is on the hunt to find an eminent experience with anything he interacts with. Any and every product he comes to contact with are to delight him not only with features but as an added edge to his overall well-being.Artisan products are taking hold of the skin care and hair care industries. Individuals are increasingly confiding in handcrafted and natural cosmetic products to enhance their well-being. They trust artisan cosmetics online than mass-produced blends of chemical potions.</div>
+    <div>Read More</div>
+    <div>
+        <div>SHIPPING PARTNERS</div>
+        <div>© deyga.in</div>
+    </div>
+    <div>
+        <img src="https://cdn.shopify.com/s/files/1/0034/7901/1441/files/shipping_2_grande.png?v=1578658420" alt=""/>
+        <img src="https://cdn.shopify.com/s/files/1/0034/7901/1441/files/shipping_1_grande.png?v=1578658404" alt=""/>
+        <img src="https://lh3.googleusercontent.com/Oodg2ViqTcQYy6Qzdb_ooxDIWKpJrzAdk-i3OYAmDeb87uL38Bg8TPQah4mzQ-PlERMX=s60" alt=""/>
+        <img src="https://lh3.googleusercontent.com/IJFYLO8dAFboFL436SRCRVM-YSorx788ffDKavo7OIqLQgAqutGEKTuUJO0F_p7GBPH0Gg=s59" alt=""/>
+        <img src="https://lh3.googleusercontent.com/F18BPirdL2ww00vpOxl7b_YUBskdQZ_7988eIXesKr-qujvldVLyrvycMn_uR6VkMFHQ9U0=s64" alt=""/>
+        <img src="https://lh3.googleusercontent.com/-Ldhmk0Iqjhd8WRpUiZW85ouFxsVF9Or378ZXrV9ioT6StOPenqrX4-yyP2gLvAzT_aAN9M=s58" alt=""/>
+        <img src="https://lh3.googleusercontent.com/4n9zhW_M2qbvx_BvrgxrMD7oleSCoyhDIuCR1FQzuE3Cadm9nxeHJd3yYvyMggbRi28Z1Iw=s79" alt=""/>
+        <img src="https://lh3.googleusercontent.com/O_QcB_WGp2GQ5cVGTxcmZW4qcP6Sm_JGMCGrKJN0AEDij3J89GizQOM3rJwImndBvPkotA=s65" alt=""/>
+        <img src="https://lh3.googleusercontent.com/JV2tBGbA1zyBXbhiAWytLAz_Io5j99ikzaAfltx7Qz4H_prPAZOM6_bFxNmkHkZVapsfwQ=s73" alt=""/>
+        <img src="https://lh3.googleusercontent.com/y4EnGi3k4iRrWJEwIH8ALNjjRlI5_l_I-UFjXmUqwAW9J1CSfCc_fib_RE9y2kj4SFXJbw=s58" alt=""/>
+        <img src="https://lh3.googleusercontent.com/aQx7M2gaN-6tYm0jXiyipSn5z4o6Xk7qbmmPR_PQUY6u_HVZT7DM_mL96rcf_LNdDAcT=s68" alt=""/>
+        <img src="https://lh3.googleusercontent.com/6_JeIfaLzYhiScYPBVlpRi8e90xwC17xhX-9wjcfJdyRlc_rUA293qY3uEjJpamRUlYl=s64" alt=""/>
+        <img src="https://lh3.googleusercontent.com/ZoYMeG1Rk9lh5mfU3Bl0gcBjRzLpcATjPgju0q4rv-LFTF3XrAzgQSfAerg9jj2M5E2eW-M=s83" alt=""/>
+        <img src="https://lh3.googleusercontent.com/VXWGQ0ciQj-XfzK-HWQ2HnWHAi-hmX3a52UOpEF1WJCEuGzMIurY9LQ3xxdhVjJc61NQDg=s73" alt=""/>
+        <img src="https://lh3.googleusercontent.com/TNtiA80JjfvktEd-n_GmTtkDMwkFLJ1lFac8g8ftTCsS2WoKbn6VE6G1bL-KK3zTCkLhqlk=s73" alt=""/>
+        <img src="https://lh3.googleusercontent.com/G0xwP9RWcUaYblyp9tD9yZsJIogiOHHTijqDAeUOiD8X1j-gs8wYqHxCqp2bI7aeSlY4gQ=s69" alt=""/>
+        <img src="https://brandlogos.net/wp-content/uploads/2013/06/visa-us-vector-logo-400x400.png" alt=""/>
+        
+    </div>`
+}
+
+export {navbar,footer}
